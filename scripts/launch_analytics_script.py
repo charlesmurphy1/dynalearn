d13e4cbe9ef27f0c6aed8c915c5d3d457a8d0855
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2019-04-17 09:07:09 -0400

    Removing data cedar_experiment.h5

--- scripts/launch_analytics_script.py
+++ scripts/launch_analytics_script.py
@@ -15,7 +15,7 @@ with open('parameters.json', 'r') as f:
     params = json.load(f)
 
 
-num_sample = 1000
+num_sample = 100
 experiment = u.get_experiment(params)
 
 h5file = h5py.File(os.path.join(params["path"],
@@ -45,7 +45,6 @@ for g in graphs:
                             states[g].shape[1],
                             dynamics.num_states))
     for i, s in enumerate(states[g][:num_sample]):
-        if i%100 == 0: print(i)
         dynamics_tp[g][i, :, :] = dynamics.predict(s)
         model_tp[g][i, :, :] = model.model.predict([s, graphs[g]], steps=1)
 
@@ -89,8 +88,8 @@ h5file.close()
 
 # Writting analytics to file
 h5file = h5py.File(os.path.join(params["path"],
-                                params["experiment_name"] + ".h5"),
-                   'a')
+                                params["experiment_name"] + "_analytics.h5"),
+                   'w')
 
 
 for g in graphs:
@@ -113,9 +112,9 @@ for g in graphs:
             avg, var = estimate_ltp[g][0][(in_s, out_s)], estimate_ltp[g][1][(in_s, out_s)]
             h5file.create_dataset(group, data=np.vstack((avg, var)).T)
 
-    # Saving attention coefficients
-    for layer in range(len(attn_layers)):
-        for in_s in dynamics.state_label:
-            for out_s in dynamics.state_label:
-                group = "/analytics/attention_coeff_" + str(layer) + "/" + in_s + "_to_" + out_s
-                h5file.create_dataset(group, data=cond_attn_coeff[layer][(in_s, out_s)])
+# Saving attention coefficients
+for in_s in dynamics.state_label:
+    for out_s in dynamics.state_label:
+        for layer in range(len(attn_layers)):
+            group = "/analytics/attention_coeff_" + str(layer) + "/" + in_s + "_to_" + out_s
+            h5file.create_dataset(group, data=cond_attn_coeff[layer][(in_s, out_s)])
