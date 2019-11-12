ebd371362e5bf06718bc09194e8675937daf3bbf
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2019-11-12 12:21:48 -0500

    Changing complex contagion threshold model with absolute values.

--- dynalearn/meanfields/complex_contagion.py
+++ dynalearn/meanfields/complex_contagion.py
@@ -9,8 +9,9 @@ def sigmoid(x):
 
 
 def inf_prob_func(mu, beta, l_grid, k_grid):
-    x = l_grid[1] / k_grid
-    return sigmoid(beta * (x - mu))
+    p = sigmoid(beta * (l_grid[1] / k_grid - mu))
+    p[k_grid == 0] = 0
+    return p
 
 
 class SoftThresholdSIS_AME(AME):
@@ -30,7 +31,7 @@ class SoftThresholdSIS_AME(AME):
         ltp[0, 0] = 1 - inf_prob
         ltp[0, 1] = inf_prob
         ltp[1, 0] = self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 1] = 1 - ltp[1, 0]
+        ltp[1, 1] = 1 - self.recovery_prob * np.ones(self.k_grid.shape)
         return ltp
 
 
@@ -51,7 +52,7 @@ class SoftThresholdSIS_PA(PA):
         ltp[0, 0] = 1 - inf_prob
         ltp[0, 1] = inf_prob
         ltp[1, 0] = self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 1] = 1 - ltp[1, 0]
+        ltp[1, 1] = 1 - self.recovery_prob * np.ones(self.k_grid.shape)
         return ltp
 
 
@@ -72,83 +73,5 @@ class SoftThresholdSIS_MF(MF):
         ltp[0, 0] = 1 - inf_prob
         ltp[0, 1] = inf_prob
         ltp[1, 0] = self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 1] = 1 - ltp[1, 0]
-        return ltp
-
-
-class SoftThresholdSIR_AME(AME):
-    def __init__(
-        self, p_k, mu, beta, recovery_prob, tol=1e-3, verbose=1, dtype="float"
-    ):
-        self.mu = mu
-        self.beta = beta
-        self.recovery_prob = recovery_prob
-        super(SoftThresholdSIR_AME, self).__init__(
-            3, p_k, tol=tol, verbose=verbose, dtype=dtype
-        )
-
-    def compute_ltp(self,):
-        ltp = np.zeros((self.s_dim, self.s_dim, *self.k_grid.shape))
-        inf_prob = inf_prob_func(self.mu, self.beta, self.l_grid, self.k_grid)
-        ltp[0, 0] = 1 - inf_prob
-        ltp[0, 1] = inf_prob
-        ltp[0, 2] = 0
-        ltp[1, 0] = 0
         ltp[1, 1] = 1 - self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 2] = 1 - ltp[1, 1]
-        ltp[2, 0] = 0
-        ltp[2, 1] = 0
-        ltp[2, 2] = 1
-        return ltp
-
-
-class SoftThresholdSIR_PA(PA):
-    def __init__(
-        self, p_k, mu, beta, recovery_prob, tol=1e-3, verbose=1, dtype="float"
-    ):
-        self.mu = mu
-        self.beta = beta
-        self.recovery_prob = recovery_prob
-        super(SoftThresholdSIR_PA, self).__init__(
-            3, p_k, tol=tol, verbose=verbose, dtype=dtype
-        )
-
-    def compute_ltp(self,):
-        ltp = np.zeros((self.s_dim, self.s_dim, *self.k_grid.shape))
-        inf_prob = inf_prob_func(self.mu, self.beta, self.l_grid, self.k_grid)
-        ltp[0, 0] = 1 - inf_prob
-        ltp[0, 1] = inf_prob
-        ltp[0, 2] = 0
-        ltp[1, 0] = 0
-        ltp[1, 1] = 1 - self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 2] = 1 - ltp[1, 1]
-        ltp[2, 0] = 0
-        ltp[2, 1] = 0
-        ltp[2, 2] = 1
-        return ltp
-
-
-class SoftThresholdSIR_MF(MF):
-    def __init__(
-        self, p_k, mu, beta, recovery_prob, tol=1e-3, verbose=1, dtype="float"
-    ):
-        self.mu = mu
-        self.beta = beta
-        self.recovery_prob = recovery_prob
-        super(SoftThresholdSIR_MF, self).__init__(
-            3, p_k, tol=tol, verbose=verbose, dtype=dtype
-        )
-
-    def compute_ltp(self,):
-        ltp = np.zeros((self.s_dim, self.s_dim, *self.k_grid.shape))
-        inf_prob = inf_prob_func(self.mu, self.beta, self.l_grid, self.k_grid)
-        ltp[0, 0] = 1 - inf_prob
-        ltp[0, 1] = inf_prob
-        ltp[0, 2] = 0
-        ltp[1, 0] = 0
-        ltp[1, 1] = 1 - self.recovery_prob * np.ones(self.k_grid.shape)
-        ltp[1, 2] = 1 - ltp[1, 1]
-        ltp[2, 0] = 0
-        ltp[2, 1] = 0
-        ltp[2, 2] = 1
         return ltp
