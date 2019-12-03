7ee97ae64fc1ceb25b496453525d97925d95702d
Charles Murphy <charles.murphy.1@ulaval.ca>
2019-12-02 21:58:15 -0500

    Test script for experiment

--- scripts/test_experiment.py
+++ scripts/test_experiment.py
@@ -2,30 +2,14 @@ import dynalearn as dl
 
 param_dict = {
     "name": "test_experiment",
-    "graph":
-    {
-        "name": "BAGraph",
-        "params":
-        {
-            "N": 1000,
-            "M": 2
-        }
-    },
-    "dynamics":
-    {
+    "graph": {"name": "BAGraph", "params": {"N": 1000, "M": 2}},
+    "dynamics": {
         "name": "SIS",
-        "params":
-        {
-            "infection_prob": 0.04,
-            "recovery_prob": 0.08,
-            "init_state": "None"
-        }
+        "params": {"infection_prob": 0.04, "recovery_prob": 0.08, "init_state": "None"},
     },
-    "model":
-    {
+    "model": {
         "name": "LocalStatePredictor",
-        "params":
-        {
+        "params": {
             "in_features": [32],
             "attn_features": [32],
             "out_features": [32],
@@ -35,44 +19,47 @@ param_dict = {
             "out_activation": "relu",
             "weight_decay": 0.0001,
             "tf_seed": 2,
-        }
+        },
     },
-    "generator":
-    {
+    "generator": {
         "name": "DynamicsGenerator",
-        "params":
-        {
+        "params": {
             "batch_size": -1,
             "num_graphs": 1,
             "num_sample": 100,
             "resampling_time": 2,
             "val_fraction": 0.01,
             "max_null_iter": 1,
-            "with_truth": 0
+            "with_truth": 0,
         },
-        "sampler":
-        {
+        "sampler": {
             "name": "StateBiasedSampler",
-            "params":
-            {
+            "params": {
                 "sampling_bias": 0.6,
                 "val_bias": 0.8,
-                "replace":1,
-                "resample":1000,
-                "sample_from_weights":0
-            }
+                "replace": 1,
+                "resample": 1000,
+                "sample_from_weights": 0,
+            },
         },
     },
-    "config":
-    {
+    "config": {
         "optimizer": "Adam",
         "initial_lr": 0.0005,
         "loss": "categorical_crossentropy",
-        "schedule": {"epoch":10, "factor":2},
+        "schedule": {"epoch": 10, "factor": 2},
         "epochs": 5,
         "np_seed": 1,
     },
-    "metrics": [],
+    "metrics": [
+        "AttentionMetrics",
+        "TrueLTPMetrics",
+        "GNNLTPMetrics",
+        "MLELTPMetrics",
+        "TrueStarLTPMetrics",
+        "GNNStarLTPMetrics",
+        "StatisticsMetrics",
+    ],
     "path_to_experiment": "./data/ba-sis/",
     "filename_data": "data.h5",
     "filename_metrics": "metrics.h5",
@@ -81,5 +68,4 @@ param_dict = {
 }
 
 experiment = dl.Experiment(param_dict, verbose=1)
-experiment.generate_data()
-experiment.train_model()
+experiment.run()
