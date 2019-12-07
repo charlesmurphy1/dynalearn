c0ef16ee024fb38c9dc501c1f46fe5dce6ffc0a8
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2019-12-07 04:52:16 -0500

    Cleaning generator, dynamics and graphs into datasets.

---
+++ dynalearn/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .generator import *
+from .getter import *
+import dynalearn.samplers
+import dynalearn.graphs
+import dynalearn.dynamics
--- dynalearn/datasets/dynamics/base.py
+++ dynalearn/datasets/dynamics/base.py
@@ -1,18 +1,106 @@
 """
 
-epidemic.py
+dynamical_network.py
 
 Created by Charles Murphy on 26-06-18.
 Copyright © 2018 Charles Murphy. All rights reserved.
 Quebec, Canada
 
-Defines DynamicalNetwork sub-classes for epidemic spreading dynamics.
+Defines the class DynamicalNetwork which generate network on which a dynamical
+process occurs.
 
 """
 
-from .dynamics import *
+import networkx as nx
 import numpy as np
-from math import ceil
+import pickle
+import os
+from abc import ABC, abstractmethod
+
+
+class Dynamics(ABC):
+    """
+		Base class for dynamical network.
+
+		**Parameters**
+		graph : nx.Graph
+			A graph on which the dynamical process occurs.
+
+		filename : String : (default = ``None``)
+			Name of file for saving activity states. If ``None``, it does not save the states.
+
+		full_data_mode : Bool : (default = ``False``)
+
+
+	"""
+
+    def __init__(self, num_states):
+        """
+		Initializes a Dynamics object.
+
+		"""
+        self._num_states = num_states
+        self._graph = None
+        self._degree = None
+
+    @abstractmethod
+    def initialize_states(self):
+        """
+		Initializes the nodes activity states. (virtual) (private)
+
+		"""
+        raise NotImplementedError("self.initialize_states() has not been impletemented")
+
+    @abstractmethod
+    def predict(self, states=None, ajd=None):
+        """
+		Computes the next activity states probability distribution. (virtual) (private)
+
+		"""
+        raise NotImplementedError("self.transition() has not been impletemented")
+
+    @abstractmethod
+    def update(self, states=None, ajd=None):
+        """
+		Computes the next activity states. (virtual) (private)
+
+		"""
+        raise NotImplementedError("self.transition() has not been impletemented")
+
+    @abstractmethod
+    def get_avg_state(self):
+        """
+		Get the average states. (virtual)
+
+		**Returns**
+		avg_state : Activity
+
+		"""
+        raise NotImplementedError("self.get_avg_states has not been implemented.")
+
+    @property
+    def graph(self):
+        if self._graph is None:
+            raise ValueError("No graph has been parsed to the dynamics.")
+        else:
+            return self._graph
+
+    @graph.setter
+    def graph(self, graph):
+        self._graph = graph
+        self._degree = np.sum(nx.to_numpy_array(graph), axis=1)
+        self.initialize_states()
+
+    @property
+    def degree(self):
+        if self._degree is None:
+            raise ValueError("No graph has been parsed to the dynamics.")
+        else:
+            return self._degree
+
+    @property
+    def num_states(self):
+        return self._num_states
 
 
 class Epidemics(Dynamics):
@@ -44,7 +132,6 @@ class Epidemics(Dynamics):
 
         avg_states = {l: np.mean(state_dict[l]) for l in state_dict}
         std_states = {l: np.std(state_dict[l]) for l in state_dict}
-        return avg_states, std_states
 
 
 class SingleEpidemics(Epidemics):
