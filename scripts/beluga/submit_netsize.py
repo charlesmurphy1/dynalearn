8b0307e1460446ba5b9dc560fb056cd66470209c
Charles Murphy <murphy9@beluga4.int.ets1.calculquebec.ca>
2020-02-23 09:48:16 -0500

    Fix density for changing num_nodes on ERgraph

--- scripts/beluga/submit_netsize.py
+++ scripts/beluga/submit_netsize.py
@@ -44,7 +44,6 @@ for nn in num_nodes:
         script += "module load python/3.6 scipy-stack mpi4py\n"
         script += "source /home/murphy9/.dynalearn-env/bin/activate\n"
         script += "python /home/murphy9/source/dynalearn/scripts/training_script.py"
-        # script += "python ss_script.py"
         script += " --config {0}".format(config)
         script += " --num_samples {0}".format(num_samples)
         script += " --num_nodes {0}".format(nn)
