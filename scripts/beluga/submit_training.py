5eb3d0128237c999f98981caae3e8eb037803a0b
Charles Murphy <charles.murphy.1@ulaval.ca>
2020-04-27 17:30:33 -0400

    Small changes to beluga scripts.

--- scripts/beluga/submit_training.py
+++ scripts/beluga/submit_training.py
@@ -1,5 +1,6 @@
 import os
 import time
+from itertools import product
 
 
 path_to_dynalearn = "/home/murphy9/source/dynalearn/"
@@ -22,8 +23,8 @@ if not os.path.exists(path_to_outputs):
     os.makedirs(path_to_outputs)
 
 num_nodes = 1000
-num_samples = [100, 500, 1000, 5000, 10000, 20000]
-configs_to_run = [
+num_samples_array = [100, 500, 1000, 5000, 10000, 20000]
+config_array = [
     "sis-er",
     "sis-ba",
     "plancksis-er",
@@ -32,45 +33,40 @@ configs_to_run = [
     "sissis-ba",
 ]
 
-for ns in num_samples:
-    for config in configs_to_run:
-        suffix = "ns" + str(ns)
-        name = config + "-" + suffix
-        script = "#!/bin/bash\n"
-        script += "#SBATCH --account=def-aallard\n"
-        script += "#SBATCH --time=12:00:00\n"
-        script += "#SBATCH --job-name={0}\n".format(name)
-        script += "#SBATCH --output={0}.out\n".format(
-            os.path.join(path_to_outputs, name)
-        )
-        script += "#SBATCH --gres=gpu:1\n"
-        script += "#SBATCH --mem=24G\n"
-        script += "\n"
-        script += "module load python/3.6 scipy-stack mpi4py\n"
-        script += "source /home/murphy9/.dynalearn-env/bin/activate\n"
-        script += "python {0}scripts/training_script.py".format(path_to_dynalearn)
-        script += " --config {0}".format(config)
-        script += " --name {0}".format(name)
-        script += " --num_samples {0}".format(ns)
-        script += " --num_nodes {0}".format(num_nodes)
-        script += " --resampling_time {0}".format(2)
-        script += " --batch_size {0}".format(10)
-        script += " --with_truth {0}".format(0)
-        script += " --run_fast {0}".format(0)
-        script += " --path {0}".format(path_to_data)
-        script += " --path_to_best {0}".format(path_to_best)
-        script += " --path_to_summary {0}".format(path_to_summary)
-        script += " --verbose 2\n"
-        script += "deactivate\n"
+for num_samples, config in product(num_samples_array, config_array):
+    suffix = "ns" + str(num_samples)
+    name = config + "-" + suffix
+    script = "#!/bin/bash\n"
+    script += "#SBATCH --account=def-aallard\n"
+    script += "#SBATCH --time=12:00:00\n"
+    script += "#SBATCH --job-name={0}\n".format(name)
+    script += "#SBATCH --output={0}.out\n".format(os.path.join(path_to_outputs, name))
+    script += "#SBATCH --gres=gpu:1\n"
+    script += "#SBATCH --mem=24G\n"
+    script += "\n"
+    script += "module load python/3.6 scipy-stack mpi4py\n"
+    script += "source /home/murphy9/.dynalearn-env/bin/activate\n"
+    script += "python {0}scripts/training_script.py".format(path_to_dynalearn)
+    script += " --config {0}".format(config)
+    script += " --name {0}".format(name)
+    script += " --num_samples {0}".format(num_samples)
+    script += " --num_nodes {0}".format(num_nodes)
+    script += " --resampling_time {0}".format(2)
+    script += " --batch_size {0}".format(1)
+    script += " --with_truth {0}".format(1)
+    script += " --run_fast {0}".format(0)
+    script += " --path {0}".format(path_to_data)
+    script += " --path_to_best {0}".format(path_to_best)
+    script += " --path_to_summary {0}".format(path_to_summary)
+    script += " --verbose 1\n"
+    script += "deactivate\n"
 
-        seed = 0
-        path_to_script = "{0}/{1}-{2}.sh".format(
-            os.path.join(path_to_dynalearn, "scripts/local/launch_scripts"),
-            config,
-            seed,
-        )
+    seed = 0
+    path_to_script = "{0}/{1}-{2}.sh".format(
+        os.path.join(path_to_dynalearn, "scripts/hector/launch_scripts"), config, seed
+    )
 
-        with open(path_to_script, "w") as f:
-            f.write(script)
+    with open(path_to_script, "w") as f:
+        f.write(script)
 
-        os.system("bash {0}".format(path_to_script))
+    os.system("sbatch {0}".format(path_to_script))
--- scripts/bernard/submit_training.py
+++ scripts/bernard/submit_training.py
@@ -1,12 +1,13 @@
 import os
 import time
+from itertools import product
 
 
 path_to_dynalearn = (
     "/home/charles/Documents/ulaval/doctorat/projects/dynalearn-all/dynalearn/"
 )
 path_to_dynalearn_data = (
-    "/home/charles/Documents/ulaval/doctorat/projects/dynalearn-all/dynalearn/data/"
+    "/home/charles/Documents/ulaval/doctorat/projects/dynalearn-all/dynalearn/data"
 )
 path_to_all = os.path.join(path_to_dynalearn_data, "training")
 path_to_data = os.path.join(path_to_all, "full_data")
@@ -24,9 +25,9 @@ if not os.path.exists(path_to_outputs):
     os.makedirs(path_to_outputs)
 
 num_nodes = 1000
-# num_samples = [100, 500, 1000, 5000, 10000, 20000]
-num_samples = [1000]
-configs_to_run = [
+# num_samples_array = [100, 500, 1000, 5000, 10000, 20000]
+num_samples_array = [1000]
+config_array = [
     # "sis-er",
     "sis-ba",
     # "plancksis-er",
@@ -35,45 +36,40 @@ configs_to_run = [
     # "sissis-ba",
 ]
 
-for ns in num_samples:
-    for config in configs_to_run:
-        suffix = "ns" + str(ns)
-        name = config + "-" + suffix
-        script = "#!/bin/bash\n"
-        script += "#SBATCH --account=def-aallard\n"
-        script += "#SBATCH --time=12:00:00\n"
-        script += "#SBATCH --job-name={0}\n".format(name)
-        script += "#SBATCH --output={0}.out\n".format(
-            os.path.join(path_to_outputs, name)
-        )
-        script += "#SBATCH --gres=gpu:1\n"
-        script += "#SBATCH --mem=24G\n"
-        script += "\n"
-        script += "module load python/3.6 scipy-stack mpi4py\n"
-        script += "source /home/murphy9/.dynalearn-env/bin/activate\n"
-        script += "python {0}scripts/training_script.py".format(path_to_dynalearn)
-        script += " --config {0}".format(config)
-        script += " --name {0}".format(name)
-        script += " --num_samples {0}".format(ns)
-        script += " --num_nodes {0}".format(num_nodes)
-        script += " --resampling_time {0}".format(2)
-        script += " --batch_size {0}".format(1)
-        script += " --with_truth {0}".format(1)
-        script += " --run_fast {0}".format(0)
-        script += " --path {0}".format(path_to_data)
-        script += " --path_to_best {0}".format(path_to_best)
-        script += " --path_to_summary {0}".format(path_to_summary)
-        script += " --verbose 1\n"
-        script += "deactivate\n"
+for num_samples, config in product(num_samples_array, config_array):
+    suffix = "ns" + str(num_samples)
+    name = config + "-" + suffix
+    script = "#!/bin/bash\n"
+    script += "#SBATCH --account=def-aallard\n"
+    script += "#SBATCH --time=12:00:00\n"
+    script += "#SBATCH --job-name={0}\n".format(name)
+    script += "#SBATCH --output={0}.out\n".format(os.path.join(path_to_outputs, name))
+    script += "#SBATCH --gres=gpu:1\n"
+    script += "#SBATCH --mem=24G\n"
+    script += "\n"
+    script += "module load python/3.6 scipy-stack mpi4py\n"
+    script += "source /home/murphy9/.dynalearn-env/bin/activate\n"
+    script += "python {0}scripts/training_script.py".format(path_to_dynalearn)
+    script += " --config {0}".format(config)
+    script += " --name {0}".format(name)
+    script += " --num_samples {0}".format(num_samples)
+    script += " --num_nodes {0}".format(num_nodes)
+    script += " --resampling_time {0}".format(2)
+    script += " --batch_size {0}".format(1)
+    script += " --with_truth {0}".format(1)
+    script += " --run_fast {0}".format(0)
+    script += " --path {0}".format(path_to_data)
+    script += " --path_to_best {0}".format(path_to_best)
+    script += " --path_to_summary {0}".format(path_to_summary)
+    script += " --verbose 1\n"
+    script += "deactivate\n"
 
-        seed = 0
-        path_to_script = "{0}/{1}-{2}.sh".format(
-            os.path.join(path_to_dynalearn, "scripts/local/launch_scripts"),
-            config,
-            seed,
-        )
+    seed = 0
+    path_to_script = "{0}/{1}-{2}.sh".format(
+        os.path.join(path_to_dynalearn, "scripts/bernard/launch_scripts"), config, seed
+    )
 
-        with open(path_to_script, "w") as f:
-            f.write(script)
+    with open(path_to_script, "w") as f:
+        f.write(script)
 
-        os.system("bash {0}".format(path_to_script))
+    os.system("bash {0}".format(path_to_script))
