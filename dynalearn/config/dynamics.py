5008a2c3925c5cfa978149948ab736e955251e34
Charles Murphy <charles.murphy.1@ulaval.ca>
2020-05-23 16:45:35 -0400

    Debug loss using clamp. Hopefully, this will resovle the Nan issue

--- dynalearn/config/dynamics.py
+++ dynalearn/config/dynamics.py
@@ -41,7 +41,7 @@ class DynamicsConfig(Config):
         cls.num_states = 2
 
         cls.loss = "weighted_cross_entropy"
-        cls.optimizer = OptimizerConfig.radam_default()
+        cls.optimizer = OptimizerConfig.adam_default()
 
         cls.in_activation = "relu"
         cls.att_activation = "relu"
