cad1ff35ebe5fc1b8ef28854d7a18f0fd1b5aa5d
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2020-10-08 14:02:45 -0400

    debug problem with size in prediction metric

--- dynalearn/experiments/metrics/prediction.py
+++ dynalearn/experiments/metrics/prediction.py
@@ -9,7 +9,6 @@ from .metrics import Metrics
 class PredictionMetrics(Metrics):
     def __init__(self, config, verbose=0):
         Metrics.__init__(self, config, verbose)
-
         self.max_num_points = config.max_num_points
         self.model = None
         self.names = ["pred", "degree", "train_pred", "train_degree"]
@@ -35,21 +34,23 @@ class PredictionMetrics(Metrics):
         self.model = self.get_model(experiment)
         self.dataset = experiment.dataset
         self.num_states = experiment.model.num_states
-
         self.points = {}
+        self.size = 0
         self.num_updates = 0
         for k, g in enumerate(self.dataset.networks.data_list):
+            n = experiment.networks.num_nodes
             if (
-                self.dataset.data["inputs"][k].size > self.max_num_points
+                self.dataset.data["inputs"][k].size > self.max_num_points // n
                 and self.max_num_points != -1
             ):
                 self.points[k] = sample(
-                    range(self.dataset.data["inputs"][k].size), self.max_num_points
+                    range(self.dataset.data["inputs"][k].size), self.max_num_points // n
                 )
+                self.size += self.max_num_points
             else:
                 self.points[k] = range(self.dataset.data["inputs"][k].size)
+                self.size += self.dataset.data["inputs"][k].size * n
             self.num_updates += len(self.points[k])
-
         self.all_nodes = self._nodes_(experiment.dataset, all=True)
         self.get_data["pred"] = lambda pb: self._pred_(self.all_nodes, pb=pb)
         self.get_data["degree"] = lambda pb: self._degree_(self.all_nodes, pb=pb)
@@ -57,20 +58,20 @@ class PredictionMetrics(Metrics):
         train_nodes = self._nodes_(experiment.dataset)
         self.get_data["train_pred"] = lambda pb: self._pred_(train_nodes, pb=pb)
         self.get_data["train_degree"] = lambda pb: self._degree_(train_nodes, pb=pb)
-        update_factor = 2
+        update_factor = 4
         if experiment.val_dataset is not None:
             val_nodes = self._nodes_(experiment.val_dataset)
             self.get_data["val_pred"] = lambda pb: self._pred_(val_nodes, pb=pb)
             self.get_data["val_degree"] = lambda pb: self._degree_(val_nodes, pb=pb)
             self.names.extend(["val_pred", "val_degree"])
-            update_factor += 1
+            update_factor += 2
 
         if experiment.test_dataset is not None:
             test_nodes = self._nodes_(experiment.test_dataset)
             self.get_data["test_pred"] = lambda pb: self._pred_(test_nodes, pb=pb)
             self.get_data["test_degree"] = lambda pb: self._degree_(test_nodes, pb=pb)
             self.names.extend(["test_pred", "test_degree"])
-            update_factor += 1
+            update_factor += 2
         self.num_updates *= update_factor
 
     def _nodes_(self, dataset, all=False):
@@ -91,7 +92,7 @@ class PredictionMetrics(Metrics):
         return nodes
 
     def _pred_(self, nodes, pb=None):
-        pred_array = np.zeros([int(self.num_updates), int(self.num_states)])
+        pred_array = np.zeros([int(self.size), int(self.num_states)])
         i = 0
         for k in range(self.dataset.data["networks"].size):
             indices = set(range(self.dataset.data["inputs"][k].size))
@@ -111,7 +112,7 @@ class PredictionMetrics(Metrics):
         return pred_array
 
     def _degree_(self, nodes, pb=None):
-        degree_array = np.zeros(int(self.num_updates))
+        degree_array = np.zeros(int(self.size))
         i = 0
         for k in range(self.dataset.data["networks"].size):
             indices = set(range(self.dataset.data["inputs"][k].size))
