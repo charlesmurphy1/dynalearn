53f1bfb15a64494e2fbde18f9c7626cf54114b91
Charles Murphy <charles.murphy.1@ulaval.ca>
2021-03-23 10:31:48 -0400

    debug univariate

--- dynalearn/dynamics/trainable/stochastic_epidemics.py
+++ dynalearn/dynamics/trainable/stochastic_epidemics.py
@@ -41,19 +41,57 @@ class GNNSEDynamics(StochasticEpidemics):
         return self.initial_state()
 
 
-class UVSEDynamics(GNNSEDynamics):
+class UVSEDynamics(StochasticEpidemics):
     def __init__(self, config=None, **kwargs):
-        GNNSEDynamics.__init__(self, config=config, **kwargs)
+        self.config = config or Config(**kwargs)
+        StochasticEpidemics.__init__(self, config, config.num_states)
         if "rnn" in config.__dict__ and config.rnn != "None":
             self.nn = StochasticEpidemicsURNN(config)
         else:
             self.nn = StochasticEpidemicsUMPL(config)
+        if torch.cuda.is_available():
+            self.nn = self.nn.cuda()
+
+    def predict(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.Tensor(x)
+        assert x.ndim == 2
+        assert x.shape[-1] == self.window_size
+        x = self.nn.transformers["t_inputs"].forward(x)
+        g = self.nn.transformers["t_networks"].forward(self.network)
+        y = self.nn.transformers["t_targets"].backward(self.nn.forward(x, g))
+        return y.cpu().detach().numpy()
+
+    def number_of_infected(self, x):
+        return np.inf
+
+    def nearly_dead_state(self, **kwargs):
+        return self.initial_state()
 
 
-class MVSEDynamics(GNNSEDynamics):
+class MVSEDynamics(StochasticEpidemics):
     def __init__(self, config=None, **kwargs):
-        GNNSEDynamics.__init__(self, config=config, **kwargs)
+        self.config = config or Config(**kwargs)
+        StochasticEpidemics.__init__(self, config, config.num_states)
         if "rnn" in config.__dict__ and config.rnn != "None":
             self.nn = StochasticEpidemicsMRNN(config)
         else:
             self.nn = StochasticEpidemicsMMPL(config)
+        if torch.cuda.is_available():
+            self.nn = self.nn.cuda()
+
+    def predict(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.Tensor(x)
+        assert x.ndim == 2
+        assert x.shape[-1] == self.window_size
+        x = self.nn.transformers["t_inputs"].forward(x)
+        g = self.nn.transformers["t_networks"].forward(self.network)
+        y = self.nn.transformers["t_targets"].backward(self.nn.forward(x, g))
+        return y.cpu().detach().numpy()
+
+    def number_of_infected(self, x):
+        return np.inf
+
+    def nearly_dead_state(self, **kwargs):
+        return self.initial_state()
